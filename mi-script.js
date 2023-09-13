e98d38efbdd25c46c8f1191e6f249ef4b6a5ff46
sapotronRonny <118615209+sapotronRonny@users.noreply.github.com>
2023-09-13 11:52:49 -0500

    Add files via upload

---
+++ mi-script.js
@@ -0,0 +1,12 @@
+var primer_saludo = "hola";
+var segundo_saludo =  primer_saludo;
+primer_saludo = "hello";
+alert(segundo_saludo);
+alert(primer_saludo);
+
+var dividendo = prompt("introduce el dividendo:");
+var divisor = prompt("introduce el divisor");
+var resultado;
+divisor !=0 ? resultado = dividendo/divisor:
+    alert("No es posible la division por cero");
+    alert("el resultado es:"+resultado);
